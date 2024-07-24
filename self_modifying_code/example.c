fa307d6b388237e6530e3e68bca2aa4b739e4e84
SamNzo <91531132+SamNzo@users.noreply.github.com>
2024-07-24 16:30:25 +0200

    Create example.c

---
+++ self_modifying_code/example.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <windows.h>
+
+char s1[] = "Original code\n";
+char s2[] = "Modified code\n";
+int (*printf_ptr)(const char *const _Format, ...) = printf;
+const char* absolute_s1 = s1;
+const char* absolute_s2 = s2;
+
+unsigned char bytes[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x04, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+void f1() {
+    printf_ptr(absolute_s1);
+}
+
+void f2() {
+    printf_ptr(absolute_s2);
+}
+
+int main(int argc, char **argv) {
+    f1();
+    void (*addr)() = f1;
+
+    DWORD oldProtect;
+    SIZE_T size = 29;
+
+    VirtualProtect(addr, size, PAGE_READWRITE, &oldProtect);
+    unsigned char* code = (unsigned char*) addr;
+    for (SIZE_T i = 0; i < size; i++) {
+        code[i] ^= bytes[i];  // XOR operation
+    }
+    VirtualProtect(addr, size, oldProtect, &oldProtect);
+
+    f1();
+}
